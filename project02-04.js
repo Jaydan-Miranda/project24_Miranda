72c883aba367d59e8c29e24e0076ac9f2db1e896
Jaydan-Miranda <miry08733@gmail.com>
2025-03-27 10:52:50 -0500

    commited

--- project02-04.js
+++ project02-04.js
@@ -3,15 +3,23 @@
       Project 02-04
 
       Application to calculate the cost of a restaurant order plus tax
-      Author: 
-      Date:   
+      Author: Jaydan Miranda
+      Date:   03/27/2025
 
       Filename: project02-04.js
  */
  
+   const CHICKEN_PRICE = 10.95
+   const HALIBUT_PRICE = 13.95
+   const BURGER_PRICE = 9.95
+   const SALMON_PRICE = 18.95
+   const SALAD = 7.95
+   const SALES_TAX = 0.07
 
-
-
+   function calcTotal() {
+      const cost = 0
+      const buyChicken = chicken
+   }
 
 // Function to display a numeric value as a text string in the format $##.## 
  function formatCurrency(value) {
